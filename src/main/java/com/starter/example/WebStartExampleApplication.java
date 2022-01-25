e68d293fcd4af52b908bcea5b5996b4680d678fa
Majuhwan <akfoddl@gmail.com>
2022-01-25 23:58:31 +0900

    initial

---
+++ src/main/java/com/starter/example/WebStartExampleApplication.java
@@ -0,0 +1,13 @@
+package com.starter.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStartExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStartExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starter/example/WebStartExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStartExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
